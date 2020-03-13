9bed959b2a2336862fd649a1fb0ca1dafee06d4d
MrPangak <aripsjen@gmail.com>
2020-03-13 21:18:14 +0800

    new commit

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -10,13 +10,13 @@ class CalculatorBrain {
   double _bmi;
 
   String calculateBMI() {
-    double _bmi = weight/pow(height / 100, 2);
+    _bmi = weight/pow(height / 100, 2);
     return _bmi.toStringAsFixed(1);
   }
 
   String getResult() {
     if(_bmi >= 25) {
-      return 'Overwight';
+      return 'Overweight';
     }
     else if(_bmi > 18.5) {
       return 'Normal';
